21d1be38ea2087a94be86bb376ffe0b796d565ba
mziele1 <mziele1@tigers.lsu.edu>
2014-05-01 20:58:15 -0500

    variable text editing fixed

--- norm.sh
+++ norm.sh
@@ -2,23 +2,22 @@
 COUNTER=0
 RMSCOUNTER=0
 
-echo "Reading files..."
 while read line; do
 RMSSTAT=$(sox $line -n stat 2>&1 | grep "RMS     amplitude:")
-#echo "RMSSTAT = \"$RMSSTAT\""
+echo "RMSSTAT = \"$RMSSTAT\""
 RMSCOUNTER=$(echo "scale=6; $RMSCOUNTER + ${RMSSTAT##*\ }" | bc)
-#echo "RMSSTAT value = \"${RMSSTAT##*\ }\" ... RMSCOUNTER updated to \"$RMSCOUNTER\""
+echo "RMSSTAT value = \"${RMSSTAT##*\ }\" ... RMSCOUNTER updated to \"$RMSCOUNTER\""
 COUNTER=$[COUNTER + 1]
-#echo "COUNTER = $COUNTER"
+echo "COUNTER = $COUNTER"
 done < fileSelection.txt
 
 MEANRMS=$(echo "scale=6; $RMSCOUNTER / $COUNTER" |bc)
-#echo "MEANRMS = \"$MEANRMS\""
+echo "MEANRMS = \"$MEANRMS\""
 
 #now to actually do stuff
-echo "Normalizing and editing..."
 while read line; do
+TEMPFILE=`echo $line | sed 's/\(.*\)\.wav/\1-temp\.wav/'`
 normalize-audio -a $MEANRMS $line
-sox $line "temp-"$line fade h 00:00:03 0 00:00:03
-mv "temp-"$line $line
+sox $line $TEMPFILE fade h 00:00:03 0 00:00:03
+mv $TEMPFILE $line
 done < fileSelection.txt
